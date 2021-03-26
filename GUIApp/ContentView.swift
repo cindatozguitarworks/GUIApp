87cd349e47a016c701dcd8a483ac4d165f219bd5
Cynthia Anderson <cind@ozguitarworks.com>
2021-03-26 16:15:49 -0700

    Initial Commit

---
+++ GUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GUIApp
+//
+//  Created by Cynthia Anderson on 3/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GUIApp/GUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GUIAppApp.swift
+//  GUIApp
+//
+//  Created by Cynthia Anderson on 3/26/21.
+//
+
+import SwiftUI
+
+@main
+struct GUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
